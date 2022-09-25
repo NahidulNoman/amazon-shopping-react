afd73aa5c928dcb56284f54daab8a958908d0776
NahidulNoman <nahidulna5@gmail.com>
2022-09-25 21:38:49 +0600

    order-details-added

--- src/components/order/Order.js
+++ src/components/order/Order.js
@@ -2,16 +2,29 @@ import React from 'react';
 import './Order.css'
 
 const Order = (props) => {
+    const {cart} =props;
+
+    // console.log(cart)
+
+    let total = 0;
+    let shipping = 0;
+    for(let carts of cart){
+        total = total + carts.price;
+        shipping = shipping + carts.shipping
+    }
+
+    let tax = +(total * 0.1).toFixed(2)
+    // console.log(typeof tax)
     return (
         <div className='order'>
             <h2>Order Summary</h2>
                 <div className='four-header'>
-                <h4>Selected Items : {props.cart.length}</h4>
-                <h4>Total Price :    ${props.cart.length}</h4>
-                <h4>Total Shopping Charge :    ${props.cart.length}</h4>
-                <h4>Tax :    ${props.cart.length}</h4>
+                <h4>Selected Items : {cart.length}</h4>
+                <h4>Total Price : ${total}</h4>
+                <h4>Total Shipping Charge : ${shipping}</h4>
+                <h4>Tax : ${tax}</h4>
                 </div>
-                <h2>Grand Total :    ${props.cart.length}</h2>
+                <h2>Grand Total : ${}</h2>
         </div>
     );
 };
