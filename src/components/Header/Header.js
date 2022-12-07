ebf23c854b2abf4b1776862b7bf0f1310932b7c8
NahidulNoman <nahidulna5@gmail.com>
2022-12-07 23:09:55 +0600

    header section added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,41 @@
-import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
-import { AuthContext } from '../../context/UserContext';
-import picture from '../../images/Logo.svg'
-import './Header.css';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/UserContext";
+import picture from "../../images/Logo.svg";
+import "./Header.css";
 
 const Header = () => {
-    const {user,logOut} = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
 
-    const removeSignOut = () => {
-        logOut()
-        .then( () => {
-        })
-        .catch( error => {
-            console.log(error)
-        })
-    }
-    return (
-        <nav className='header'>
-            <img src={picture} alt="" />
-            <div>
-            {/* className=
+  const removeSignOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <nav className="header">
+      <img src={picture} alt="" />
+      <div>
+        {/* className=
                 {({isActive})=> isActive ? 'active' : undefined} to="./shop" */}
-                <span className='text'>{user?.email}</span>
-                <NavLink to="./shop">Shop</NavLink>
-                <NavLink to="./order">Order</NavLink>
-                <NavLink to="./inventory">Inventory</NavLink>
-                <NavLink to="./about">About</NavLink>
-                {
-                    user?.email ? 
-                    <button onClick={removeSignOut}>Sign Out</button>
-                    :
-                <>
-                <NavLink to="./signup">SignUp</NavLink>
-                <NavLink to="./login">LogIn</NavLink>
-                </>
-                }
-            </div>
-        </nav>
-    );
+        <span className="text">{user?.email}</span>
+        <NavLink to="./shop">Shop</NavLink>
+        <NavLink to="./order">Order</NavLink>
+        <NavLink to="./inventory">Inventory</NavLink>
+        <NavLink to="./about">About</NavLink>
+        {user?.email ? (
+          <button onClick={removeSignOut}>Sign Out</button>
+        ) : (
+          <>
+            <NavLink to="./signup">SignUp</NavLink>
+            <NavLink to="./login">LogIn</NavLink>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
