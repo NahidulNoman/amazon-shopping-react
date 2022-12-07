e8d0634f0fb39afa05f6ca8921fc78ecb68ba188
NahidulNoman <nahidulna5@gmail.com>
2022-12-07 23:10:28 +0600

    errorpage modify

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-    const error = useRouteError();
-    console.log(error);
-    return (
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1 className="">Ops! An Error Ocurred!</h1>
+      {error && (
         <div>
-            <h1 className=''>Ops! An Error Ocurred!</h1>
-            {error && (
-          <div>
-            <p className=''>{error.statusText || error.message}</p>
-            <p>{error.status}</p>
-          </div>
-        )}
+          <p className="">{error.statusText || error.message}</p>
+          <p>{error.status}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ErrorPage;
