9e25e19274849c66872daa0681c3b4f30e19ad3a
NahidulNoman <nahidulna5@gmail.com>
2022-12-07 23:11:00 +0600

    login page more midify

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,48 +1,50 @@
-import React, { useContext } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../context/UserContext';
-import './login.css';
+import React, { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/UserContext";
+import "./login.css";
 
 const LogIn = () => {
-    const {signUp} = useContext(AuthContext);
-    const navigate = useNavigate();
-    const location = useLocation();
+  const { signUp } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const from = location.state?.from?.pathname || '/';
+  const from = location.state?.from?.pathname || "/";
 
-    const handlerSubmit = (event) => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email,password);
+  const handlerSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
 
-        signUp(email,password)
-        .then(result => {
-            const user = result.user
-            console.log(user)
-            navigate(from, {replace : true})
-        })
-        .catch(error => console.log(error))
-    }
+    signUp(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        navigate(from, { replace: true });
+      })
+      .catch((error) => console.log(error));
+  };
 
-    return (
-        <div className='form-container'>
-        <h2 className='form-title'>Login</h2>
-        <form onSubmit={handlerSubmit}>
-            <div className="form-control">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" required />
-            </div>
-            <div className="form-control">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" required />
-            </div>
-            <input className='btn-submit' type="submit" value="Login" />
-        </form>
-        <p>New to ema john <Link to='/signup'>Create a New Account</Link></p>
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <form onSubmit={handlerSubmit}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+      <p>
+        New to ema john <Link to="/signup">Create a New Account</Link>
+      </p>
     </div>
-    );
+  );
 };
 
 export default LogIn;
