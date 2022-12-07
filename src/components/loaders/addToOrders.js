4ceab33bd5a4fecd267ef27779b5039c4b0076e9
NahidulNoman <nahidulna5@gmail.com>
2022-12-07 23:09:12 +0600

    add to orders modify

--- src/components/loaders/addToOrders.js
+++ src/components/loaders/addToOrders.js
@@ -1,23 +1,23 @@
 import { getStoreCart } from "../../utilities/fakedb";
 
 export const addToOrders = async () => {
-    // get data
-    const productData = await fetch('http://localhost:5000/products');
-    const {products} = await productData.json();
+  // get data
+  const productData = await fetch("http://localhost:5000/products");
+  const { products } = await productData.json();
 
-    //get cart
-    const saveCart = getStoreCart();
-    const initialCart = [];
+  //get cart
+  const saveCart = getStoreCart();
+  const initialCart = [];
 
-    for(const id in saveCart){
-        // console.log(id);
-        const addProduct = products.find(product => product._id === id)
-        if(addProduct){
-            const quantity = saveCart[id];
-            addProduct.quantity = quantity;
-            initialCart.push(addProduct);
-        }
+  for (const id in saveCart) {
+    // console.log(id);
+    const addProduct = products.find((product) => product._id === id);
+    if (addProduct) {
+      const quantity = saveCart[id];
+      addProduct.quantity = quantity;
+      initialCart.push(addProduct);
     }
+  }
 
-    return {products:products,initialCart:initialCart};
-}
+  return { products: products, initialCart: initialCart };
+};
